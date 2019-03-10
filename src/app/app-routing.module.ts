1fe8046ed7034c7673be3a2c176a6b10a7902ca9
David Kisari <kisari.david@gmail.com>
2019-03-10 14:36:35 +0100

    Routing uptade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "login" },
+  { path: "login", component: LoginComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HourlyChartComponent } from './dashboard/hourly-chart/hourly-chart.component';
-import { ParkingTableComponent } from './dashboard/parking-table/parking-table.component';
-import { AddParkingDialogComponent } from './dashboard/add-parking-dialog/add-parking-dialog.component';
-import { ViewParkingDialogComponent } from './dashboard/view-parking-dialog/view-parking-dialog.component';
-import { PlateQrComponent } from './dashboard/view-parking-dialog/plate-qr/plate-qr.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatFormFieldModule,
+  MatSelectModule,
+  MatButtonModule
+} from "@angular/material";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HourlyChartComponent } from "./dashboard/hourly-chart/hourly-chart.component";
+import { ParkingTableComponent } from "./dashboard/parking-table/parking-table.component";
+import { AddParkingDialogComponent } from "./dashboard/add-parking-dialog/add-parking-dialog.component";
+import { ViewParkingDialogComponent } from "./dashboard/view-parking-dialog/view-parking-dialog.component";
+import { PlateQrComponent } from "./dashboard/view-parking-dialog/plate-qr/plate-qr.component";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +28,18 @@ import { PlateQrComponent } from './dashboard/view-parking-dialog/plate-qr/plate
     ParkingTableComponent,
     AddParkingDialogComponent,
     ViewParkingDialogComponent,
-    PlateQrComponent
+    PlateQrComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
