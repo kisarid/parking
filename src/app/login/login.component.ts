f530d7e1df213c5b738030c14845efe9d690b56d
David Kisari <kisari.david@gmail.com>
2019-03-10 14:38:04 +0100

    Login screen working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ParkingService } from "../services/parking.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  attendants: string[];
+  selectedUser: string;
 
-  constructor() { }
+  constructor(private parkingService: ParkingService, private router: Router) {}
 
   ngOnInit() {
+    this.parkingService.getAttendantList().then(result => {
+      this.attendants = result;
+    });
   }
 
+  loginWithSelectedUser(): void {
+    if (this.selectedUser) {
+      this.router.navigateByUrl("dashboard");
+    }
+  }
 }
