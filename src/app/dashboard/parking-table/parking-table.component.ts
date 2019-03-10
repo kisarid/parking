5f97ddce057567afc7e26c2aa07796e1854e27b5
David Kisari <kisari.david@gmail.com>
2019-03-10 21:48:34 +0100

    Parking table draft

--- src/app/dashboard/parking-table/parking-table.component.ts
+++ src/app/dashboard/parking-table/parking-table.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from "@angular/core";
+import { MatSort, MatTableDataSource } from "@angular/material";
+
+import { Parking } from "src/app/interfaces/parking";
 
 @Component({
-  selector: 'app-parking-table',
-  templateUrl: './parking-table.component.html',
-  styleUrls: ['./parking-table.component.scss']
+  selector: "app-parking-table",
+  templateUrl: "./parking-table.component.html",
+  styleUrls: ["./parking-table.component.scss"]
 })
 export class ParkingTableComponent implements OnInit {
+  @Input() parkings: Parking[];
+  displayedColumns: string[] = ["name", "email", "date", "plate", "options"];
+  dataSource: MatTableDataSource<Parking>;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.parkings);
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  filterParking(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
 }
